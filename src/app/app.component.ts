539e1128df0a584aefb645a4c4368acd29e07804
Tunmbi Banto <hellotunmbi@gmail.com>
2021-08-02 11:57:20 +0100

    update to firebase notification fxn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,9 @@ import { FCM } from "cordova-plugin-fcm-with-dependecy-updated/ionic/ngx";
   styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
+  hasPermission = false;
+  token = "";
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -26,34 +29,58 @@ export class AppComponent {
       this.splashScreen.hide();
     });
 
-    this.getToken();
+    // this.getToken();
 
-    // subscribe to a topic
-    this.fcm.subscribeToTopic("Deals");
+    // // subscribe to a topic
+    // this.fcm.subscribeToTopic("Deals");
 
-    // ionic push notification example
-    this.fcm.onNotification().subscribe((data) => {
-      console.log("Push Data", data);
-      if (data.wasTapped) {
-        console.log("Received in background");
-      } else {
-        console.log("Received in foreground");
-      }
-    });
+    // // ionic push notification example
+    // this.fcm.onNotification().subscribe((data) => {
+    //   console.log("Push Data", data);
+    //   if (data.wasTapped) {
+    //     console.log("Received in background");
+    //   } else {
+    //     console.log("Received in foreground");
+    //   }
+    // });
 
-    // refresh the FCM token
-    this.fcm.onTokenRefresh().subscribe((token) => {
-      console.log("Token refreshed", token);
-    });
+    // // refresh the FCM token
+    // this.fcm.onTokenRefresh().subscribe((token) => {
+    //   console.log("Token refreshed", token);
+    // });
 
-    // unsubscribe from a topic
-    this.fcm.unsubscribeFromTopic("offers");
+    // // unsubscribe from a topic
+    // this.fcm.unsubscribeFromTopic("offers");
+  }
+
+  private async pushSetup() {
+    console.log("FCM SETUP INIT");
+    if (!this.platform.is("cordova")) {
+      return;
+    }
+
+    console.log("IN CORDOVA");
+
+    this.hasPermission = await this.fcm.requestPushPermission();
+    console.log("CHECK hasPermission:", this.hasPermission);
+
+    this.token = await this.fcm.getToken();
+    console.log("CHECK getToken: " + this.token);
+
+    console.log("ON NOTIFICATION SUBSCRIBE");
+    this.fcm
+      .onTokenRefresh()
+      .subscribe((newToken) => console.log("NEW TOKEN:", newToken));
+    this.fcm
+      .onNotification()
+      .subscribe((payload: object) => console.log("ON NOTIFICATION:", payload));
   }
 
   async getToken() {
     // get FCM token
     try {
       const fcmToken: string = await this.fcm.getToken();
+      return fcmToken;
       console.log("new token", fcmToken);
     } catch (err) {
       console.log("couldnt get token", err);
